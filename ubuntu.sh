059932b492afe47908316ae0e600309383b23e45
keisuke kiuchi <minismahn@yahoo.co.jp>
2013-10-10 12:03:35 +0900

    rubyのコマンドを修正

--- ubuntu.sh
+++ ubuntu.sh
@@ -45,12 +45,8 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
 #
 # 2. Ruby
 #
-mkdir /tmp/ruby && cd /tmp/ruby
-curl --progress ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz | tar xz
-cd ruby-2.0.0-p247
-./configure
-make
-sudo make install
+apt-get install -y ruby2.0.0 ruby-dev rubygems
+apt-get remove -y ruby1.8
 
 #
 # 3.Gem
